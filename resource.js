7aa8de8ed5fc1cf75348dab4d88adbb4bd61bc45
tjames192 <tjames192@gmail.com>
2024-01-18 22:44:18 -0800

    Update resource.js

--- resource.js
+++ resource.js
@@ -3,10 +3,10 @@ example scriptlets for ublock
 https://raw.githubusercontent.com/gorhill/uBlock/784ebb09050cb6617bd857f7c6a4311ac9649ce9/assets/resources/scriptlets.js
 */
 /// redirect.js
-/// alias redirect.js
+/// alias rd.js
 /**
  * @example
- * www.reddit.com##+js(redirect, https://old.reddit.com)
+ * www.reddit.com##+js(rd, https://old.reddit.com)
  * */
 (function() {
     "use strict";
