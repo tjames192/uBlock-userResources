3c7758c2834dbefe4e9df01bea99887da692ba89
tjames192 <tjames192@gmail.com>
2024-01-18 22:52:45 -0800

    Update resource.js

--- resource.js
+++ resource.js
@@ -2,18 +2,15 @@
 example scriptlets for ublock
 https://raw.githubusercontent.com/gorhill/uBlock/784ebb09050cb6617bd857f7c6a4311ac9649ce9/assets/resources/scriptlets.js
 */
+"use strict";
 
+/// replace-window-location.js
+/// alias rwl.js
+/**
+* @example
+* www.reddit.com##+js(rwl, https://old.reddit.com)
+* */
 (function() {
-    "use strict";
-    
-    /// replace-window-location.js
-    /// alias rwl.js
-    /**
-    * @example
-    * www.reddit.com##+js(rwl, https://old.reddit.com)
-    * */
-    (function() {
-        const host = "{{1}}";
-        window.location.replace(host + window.location.pathname);
-    })();
+    const host = "{{1}}";
+    window.location.replace(host + window.location.pathname);
 })();
