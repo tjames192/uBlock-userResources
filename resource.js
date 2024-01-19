862067008dfc1a0722a6abf446891251d6653578
tjames192 <tjames192@gmail.com>
2024-01-18 22:51:14 -0800

    Update resource.js

--- resource.js
+++ resource.js
@@ -2,14 +2,18 @@
 example scriptlets for ublock
 https://raw.githubusercontent.com/gorhill/uBlock/784ebb09050cb6617bd857f7c6a4311ac9649ce9/assets/resources/scriptlets.js
 */
-/// redirect.js
-/// alias rd.js
-/**
- * @example
- * www.reddit.com##+js(rd, https://old.reddit.com)
- * */
+
 (function() {
     "use strict";
-    const host = "{{1}}";
-    window.location.replace(host + window.location.pathname);
+    
+    /// replace-window-location.js
+    /// alias rwl.js
+    /**
+    * @example
+    * www.reddit.com##+js(rwl, https://old.reddit.com)
+    * */
+    (function() {
+        const host = "{{1}}";
+        window.location.replace(host + window.location.pathname);
+    })();
 })();
